756496731a210bf5b4d98acb0ba2ef39361ffb63
dahue <adriantissera@gmail.com>
2016-09-25 04:36:10 -0300

    Added SymbolTable, TypeCheckVisitor and Error classes

---
+++ src/main/java/visitor/Error.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main.java.visitor;
+
+/**
+ *
+ * @author Adrian Tissera
+ */
+public class Error {
+	private int lineNumber;
+	private int colNumber;
+	private String description;
+	
+	
+	public Error(int ln, int cn, String desc){
+		this.lineNumber = ln;
+		this.colNumber = cn;
+		this.description = desc;
+	}
+	
+}
---
+++ src/main/java/visitor/SymbolTable.java
@@ -0,0 +1,21 @@
+/*
+ * Taller de Diseño de software 2016
+ * 
+ * Proyecto: C-TDS compiler
+ * 
+ * Autor: Adrian Tissera
+ * 
+ */
+package main.java.visitor;
+
+import java.util.List;
+import java.util.Stack;
+import main.java.ast.AST;
+
+/**
+ *
+ * @author Adrian Tissera
+ */
+public class SymbolTable extends Stack<List<AST>>{
+	
+}
